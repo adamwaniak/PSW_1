202030d637008b568ee9cd8520ecd56b220b3651
Filip <filip.jarzebski@onet.eu>
2017-11-14 21:30:59 +0100

    JS done. Added writeln(), addEventListener(), parseInt(), case and while

--- pizza/pizza.js
+++ pizza/pizza.js
@@ -1,5 +1,9 @@
 checkedPizzas = [];
 
+function start(){
+    var button = document.getElementById("order-button");
+    button.addEventListener("click", buy, false);
+}
 
 function buy() {
     checkOrder();
@@ -12,6 +16,8 @@ function buy() {
         window.alert('Nic nie zamówiono');
         document.getElementById('order-details-body').innerHTML = '';
         document.getElementById('order-details-header').innerHTML = '';
+        document.getElementById("order-details-address").innerHTML = '';
+        document.getElementById("customers-points").innerHTML = '';
     }
 
 }
@@ -37,15 +43,22 @@ function fillOrderDetails() {
     var content = '';
     var sum = 0;
     var dostawa = Math.floor((Math.random() * 10) + 1);
-    for (var i = 0; i < len; i++) {
-        content += '<div style="width: 300px; float: left">' + checkedPizzas[i] + '</div>' + '<div style= " text-align: right">' + prices[i] + '</div>'
-        sum += prices[i];
-
+    var i =0;
+    while (i < len) {
+        content += '<div style="width: 300px; float: left">' + checkedPizzas[i] + '</div>' + '<div style= " text-align: right">' + parseInt(prices[i]) + '</div>'
+        sum += parseInt(prices[i]);
+        i++;
     }
     content += '<div style= " text-align: right;font-weight: 600">Suma + dostawa:</div>';
     content += '<div style= " text-align: right">' + sum + "  +  " + dostawa + "  =  " + (sum + dostawa) + '</div>';
-    document.getElementById("order-details-address").innerHTML = 'Zamówienie zostanie wysłane na adres: <br>' + address;
+    if(address == null){
+        document.getElementById("order-details-address").innerHTML = 'Zamówienie zostanie wysłane na adres: <br>' + " ";
+    }
+    else{
+        document.getElementById("order-details-address").innerHTML = 'Zamówienie zostanie wysłane na adres: <br>' + address;
+    }
     document.getElementById('order-details-body').innerHTML = content;
+    countPoints(sum);
 }
 
 function getPrices() {
@@ -55,8 +68,23 @@ function getPrices() {
     for (var i = 0; i < len; i++) {
         labelText = document.getElementById(checkedPizzas[i] + '-label').textContent.substring(14);
         prices[i] = parseFloat(labelText);
-
     }
     return prices;
 }
 
+function countPoints(sum) {
+    switch(true){
+        case (sum < 50):
+            document.getElementById("customers-points").innerHTML = '';
+            break;
+        case (sum > 49 && sum < 80):
+            document.getElementById("customers-points").innerHTML = 'Dodatkowo otrzymujesz <b>1 punkt</b> na swoją Kartę Klienta';
+            break;
+        case(sum > 79 && sum < 110):
+            document.getElementById("customers-points").innerHTML = 'Dodatkowo otrzymujesz <b>2 punkty</b> na swoją Kartę Klienta';
+            break;
+        case(sum > 109 && sum < 150):
+            document.getElementById("customers-points").innerHTML = 'Dodatkowo otrzymujesz <b>3 punkty</b> na swoją Kartę Klienta';
+            break;
+    }
+}
