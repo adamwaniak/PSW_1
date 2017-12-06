ed7b6e3300d6903fc1320f005619c0654c2e7473
Filip <filip.jarzebski@onet.eu>
2017-12-06 22:31:18 +0100

    PHP 1 done

--- phpTut.php
+++ phpTut.php
@@ -40,7 +40,21 @@ echo '<br> Długość tablicy: '. count($var);
 
 echo '<br> $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43"); <br>';
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-echo "Peter is " . $age['Peter'] . " years old.";
+echo key($age)." is " . $age['Peter'] . " years old.";
+
+$people = array("Peter", "Ben", "Joe");
+echo '<br>'."Person, who is second on the list is ". next($people).".";
+reset($people);
+$sentence = current($people)." is younger than Ben.";
+echo '<br>'.$sentence;
+
+$word = '/Ben/';
+$replacement = 'Joe';
+echo '<br>'.preg_replace($word, $replacement, $sentence).'<br>'.'<br>';
+
+define("STALA", "Zdanie jako stała.", true);
+echo stala.'<br>';
+die("Skrypt zakończony przez funkcje die().");
 
 ?>
 
